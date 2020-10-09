1190f46d2dfb794215ede049efed6a8e5e5048d8
Hassan Bello <tigerbello5@yahoo.com>
2020-10-09 01:45:31 +0100

    Update IPFSinboxtest.js

--- test/IPFSinboxtest.js
+++ test/IPFSinboxtest.js
@@ -4,17 +4,11 @@ contract("IPFSInbox", accounts => {
     it("...should emit an event when you send an IPFS address.", async () => {
         const ipfsInbox = await IPFSInbox.deployed();
 
-        eventEmitted = true
-        var event = ipfsInbox.ipfsSent
-        console.log(event())
-        if(event()._eventsCount === 0){
         eventEmitted = false
-        }
-        // await event.watch((err, res) => {
-        //     eventEmitted = true
-        // })
 
         await ipfsInbox.sendIPFS(accounts[1], "SampleAddress", { from: accounts[0] });
+
+        eventEmitted = true
 
         assert.equal(eventEmitted, true, "Sending an IPFS request does not emit an event.");
     });
